45b531c6118ffe67607e1ed59f016a87559101a6
gil <gil@wix.com>
2016-02-07 19:43:50 +0200

    Ex 2

--- app/scripts/controllers/mainController.js
+++ app/scripts/controllers/mainController.js
@@ -2,5 +2,21 @@
 
 angular.module('bookmarksApp')
   .controller('mainController', function ($scope) {
-    $scope.message = 'Hello Bookmarks app';
+    $scope.bookmarks = [
+      {
+        id: '1',
+        title: 'bookmark 1',
+        creationDate: (new Date(2016, 1, 20)).getTime()
+      },
+      {
+        id: '2',
+        title: 'bookmark 2',
+        creationDate: (new Date(2016, 1, 21)).getTime()
+      },
+      {
+        id: '3',
+        title: 'bookmark 3',
+        creationDate: (new Date(2016, 1, 22)).getTime()
+      }
+    ];
   });
