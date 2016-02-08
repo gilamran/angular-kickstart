d2a6823dfefad03d21cc1932e8aaa4932e8dfb9b
gil <gil@wix.com>
2016-02-08 20:08:46 +0200

    Ex 13

--- app/scripts/services/BookmarksService.js
+++ app/scripts/services/BookmarksService.js
@@ -1,33 +1,17 @@
 'use strict';
 
 angular.module('bookmarksApp')
-  .service('BookmarksService', function ($q) {
-    var bookmarks = [
-      {
-        id: '1',
-        title: 'bookmark 1',
-        creationDate: (new Date(2016, 1, 25)).getTime()
-      },
-      {
-        id: '2',
-        title: 'bookmark 2',
-        creationDate: (new Date(2016, 1, 21)).getTime()
-      },
-      {
-        id: '3',
-        title: 'bookmark 3',
-        creationDate: (new Date(2016, 1, 22)).getTime()
-      }
-    ];
+  .service('BookmarksService', function ($http) {
+    var bookmarks = [];
+    var loadingPromise;
 
     this.loadBookmarks = function () {
-      var deferred = $q.defer();
-
-      setTimeout(function () {
-        deferred.resolve(bookmarks);
-      }, 1000);
-
-      return deferred.promise;
+      if (!loadingPromise) {
+        loadingPromise = $http.get('data/bookmarks.json').then(function(response) {
+          return response.data;
+        });
+      }
+      return loadingPromise;
     };
 
     this.addBookmark = function () {
